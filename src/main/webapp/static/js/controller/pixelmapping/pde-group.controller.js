2372f782b35a2e2413a671970cb51a4b1f5e373f
Yi Zhao <yizhao.cs@gmail.com>
2016-05-02 11:17:30 -0700

    4

--- src/main/webapp/static/js/controller/pixelmapping/pde-group.controller.js
+++ src/main/webapp/static/js/controller/pixelmapping/pde-group.controller.js
@@ -77,125 +77,126 @@ app.controller('editSameGroup', function ($scope, $rootScope, $location, $routeP
         //$scope.listOfOrders = null;
 
         //  The user has selected a Customer from our Drop Down List.  Let's load this Customer's records.
-        var result = pixelmappingService.getRule($scope.key_id).success(function (backendData) {
+        pixelmappingService.getRule($scope.key_id).success(function (backendData) {
 
-                var processedResponseBackupRuleData = {
-                    "parseRule": null,
-                    "conditionRule": null,
-                    "actionRule": null,
-                    "gid": null,
-                    "keyId": null,
-                    "priority": null,
-                    "type": null,
-                    "split1": {
-                        "column1": null
-                    },
-                    "split2": {
-                        "column1": null,
-                        "column2": null
-                    },
-                    "len": {
-                        "column1": null
-                    },
-                    "range": {
-                        "column1": null,
-                        "column2": null
-                    },
-                    "substr": {
-                        "column1": null,
-                        "column2": null,
-                        "column3": null
-                    },
-                    "dec": {
-                        "column1": null
-                    },
-                    "inElementArray": [],
-                    "setRuleArray": []
-                };
+            $scope.buttonText = 'Update Group';
+            $scope.isUpdate = (backendData.key_id > 0) ? true : false;
 
-                // true if user click on the "edit button"
-                if (gid != 0) {
-                    processedResponseBackupRuleData.gid = backendData.gid;
-                    processedResponseBackupRuleData.keyId = backendData.key_id;
-                    processedResponseBackupRuleData.type = backendData.type;
-                    processedResponseBackupRuleData.priority = backendData.priority;
 
-                    var parseRuleSplit = backendData.parse_rule.split("|");
-                    processedResponseBackupRuleData.parseRule = parseRuleSplit[0];
-                    if (parseRuleSplit[0] == 'split1') {
-                        // true if -> -> split1|"|"
-                        if (parseRuleSplit.length == 3) {
-                            processedResponseBackupRuleData.split1.column1 = "\"|\"";
-                        } else {
-                            processedResponseBackupRuleData.split1.column1 = parseRuleSplit[1];
-                        }
-                    } else if (parseRuleSplit[0] == 'split2') {
-                        /*
-                         * handle case of -> split2|"|"|"|"
-                         * handle case of -> split2|"|"|,
-                         * handle case of -> split2|,|"|"
-                         * */
-                        if (parseRuleSplit.length == 5) { // true if -> split2|"|"|"|"
+            var processedResponseBackupRuleData = {
+                "parseRule": null,
+                "conditionRule": null,
+                "actionRule": null,
+                "gid": null,
+                "keyId": null,
+                "priority": null,
+                "type": null,
+                "split1": {
+                    "column1": null
+                },
+                "split2": {
+                    "column1": null,
+                    "column2": null
+                },
+                "len": {
+                    "column1": null
+                },
+                "range": {
+                    "column1": null,
+                    "column2": null
+                },
+                "substr": {
+                    "column1": null,
+                    "column2": null,
+                    "column3": null
+                },
+                "dec": {
+                    "column1": null
+                },
+                "inElementArray": [],
+                "setRuleArray": []
+            };
+
+            // true if user click on the "edit button"
+            if (gid != 0) {
+                processedResponseBackupRuleData.gid = backendData.gid;
+                processedResponseBackupRuleData.keyId = backendData.key_id;
+                processedResponseBackupRuleData.type = backendData.type;
+                processedResponseBackupRuleData.priority = backendData.priority;
+
+                var parseRuleSplit = backendData.parse_rule.split("|");
+                processedResponseBackupRuleData.parseRule = parseRuleSplit[0];
+                if (parseRuleSplit[0] == 'split1') {
+                    // true if -> -> split1|"|"
+                    if (parseRuleSplit.length == 3) {
+                        processedResponseBackupRuleData.split1.column1 = "\"|\"";
+                    } else {
+                        processedResponseBackupRuleData.split1.column1 = parseRuleSplit[1];
+                    }
+                } else if (parseRuleSplit[0] == 'split2') {
+                    /*
+                     * handle case of -> split2|"|"|"|"
+                     * handle case of -> split2|"|"|,
+                     * handle case of -> split2|,|"|"
+                     * */
+                    if (parseRuleSplit.length == 5) { // true if -> split2|"|"|"|"
+                        processedResponseBackupRuleData.split2.column1 = "\"|\"";
+                        processedResponseBackupRuleData.split2.column2 = "\"|\"";
+                    } else if (parseRuleSplit.length == 4) {
+                        // true if -> split2|"|"|,
+                        // false if -> split2|,|"|"
+                        if (parseRuleSplit[1] == "\"") {
                             processedResponseBackupRuleData.split2.column1 = "\"|\"";
-                            processedResponseBackupRuleData.split2.column2 = "\"|\"";
-                        } else if (parseRuleSplit.length == 4) {
-                            // true if -> split2|"|"|,
-                            // false if -> split2|,|"|"
-                            if (parseRuleSplit[1] == "\"") {
-                                processedResponseBackupRuleData.split2.column1 = "\"|\"";
-                                processedResponseBackupRuleData.split2.column2 = parseRuleSplit[3];
-                            } else {
-                                processedResponseBackupRuleData.split2.column1 = parseRuleSplit[1];
-                                processedResponseBackupRuleData.split2.column2 = "\"|\"";
-                            }
+                            processedResponseBackupRuleData.split2.column2 = parseRuleSplit[3];
                         } else {
                             processedResponseBackupRuleData.split2.column1 = parseRuleSplit[1];
-                            processedResponseBackupRuleData.split2.column2 = parseRuleSplit[2];
+                            processedResponseBackupRuleData.split2.column2 = "\"|\"";
                         }
-                    }
-
-
-                    var conditionRuleSplit = backendData.condition_rule.split("|");
-                    processedResponseBackupRuleData.conditionRule = conditionRuleSplit[0];
-                    if (conditionRuleSplit[0] == 'len') {
-                        processedResponseBackupRuleData.len.column1 = conditionRuleSplit[1];
-                    } else if (conditionRuleSplit[0] == 'range') {
-                        processedResponseBackupRuleData.range.column1 = conditionRuleSplit[1];
-                        processedResponseBackupRuleData.range.column2 = conditionRuleSplit[2];
-                    } else if (conditionRuleSplit[0] == 'in') {
-                        for (var i = 1; i < conditionRuleSplit.length; i++) {
-                            var inObject = {
-                                column1: i - 1,
-                                column2: conditionRuleSplit[i]
-                            }
-                            processedResponseBackupRuleData.inElementArray.push(inObject);
-                        }
-                    }
-
-                    var actionRuleSplit = backendData.action_rule.split("|");
-                    processedResponseBackupRuleData.actionRule = actionRuleSplit[0];
-                    if (actionRuleSplit[0] == 'substr') {
-                        processedResponseBackupRuleData.substr.column1 = actionRuleSplit[1];
-                        processedResponseBackupRuleData.substr.column2 = actionRuleSplit[2];
-                        processedResponseBackupRuleData.substr.column3 = actionRuleSplit[3];
-                    } else if (actionRuleSplit[0] == 'set') {
-                        for (var i = 1; i < actionRuleSplit.length; i++) {
-                            var setObject = {
-                                column1: i - 1,
-                                column2: actionRuleSplit[i]
-                            }
-                            processedResponseBackupRuleData.setRuleArray.push(setObject);
-                        }
-                    } else if (actionRuleSplit[0] == 'dec') {
-                        processedResponseBackupRuleData.dec.column1 = actionRuleSplit[1];
+                    } else {
+                        processedResponseBackupRuleData.split2.column1 = parseRuleSplit[1];
+                        processedResponseBackupRuleData.split2.column2 = parseRuleSplit[2];
                     }
                 }
 
-                $scope.rightHandPanelData = angular.copy(processedResponseBackupRuleData);
-            })
-            .error(function (data, status, headers, config) {
-                $scope.errorMessage = "Couldn't load the list of Orders, error # " + status;
-            });
+
+                var conditionRuleSplit = backendData.condition_rule.split("|");
+                processedResponseBackupRuleData.conditionRule = conditionRuleSplit[0];
+                if (conditionRuleSplit[0] == 'len') {
+                    processedResponseBackupRuleData.len.column1 = conditionRuleSplit[1];
+                } else if (conditionRuleSplit[0] == 'range') {
+                    processedResponseBackupRuleData.range.column1 = conditionRuleSplit[1];
+                    processedResponseBackupRuleData.range.column2 = conditionRuleSplit[2];
+                } else if (conditionRuleSplit[0] == 'in') {
+                    for (var i = 1; i < conditionRuleSplit.length; i++) {
+                        var inObject = {
+                            column1: i - 1,
+                            column2: conditionRuleSplit[i]
+                        }
+                        processedResponseBackupRuleData.inElementArray.push(inObject);
+                    }
+                }
+
+                var actionRuleSplit = backendData.action_rule.split("|");
+                processedResponseBackupRuleData.actionRule = actionRuleSplit[0];
+                if (actionRuleSplit[0] == 'substr') {
+                    processedResponseBackupRuleData.substr.column1 = actionRuleSplit[1];
+                    processedResponseBackupRuleData.substr.column2 = actionRuleSplit[2];
+                    processedResponseBackupRuleData.substr.column3 = actionRuleSplit[3];
+                } else if (actionRuleSplit[0] == 'set') {
+                    for (var i = 1; i < actionRuleSplit.length; i++) {
+                        var setObject = {
+                            column1: i - 1,
+                            column2: actionRuleSplit[i]
+                        }
+                        processedResponseBackupRuleData.setRuleArray.push(setObject);
+                    }
+                } else if (actionRuleSplit[0] == 'dec') {
+                    processedResponseBackupRuleData.dec.column1 = actionRuleSplit[1];
+                }
+            }
+
+            $scope.frontendRightHandPanelData = angular.copy(processedResponseBackupRuleData);
+        });
         //$http.get('http://inorthwind.azurewebsites.net/Service1.svc/getBasketsForCustomer/' + $scope.selectedCustomer)
         //    .success(function (data) {
         //        $scope.listOfOrders = data.GetBasketsForCustomerResult;
