f8b1e27922ddd11e842f5b7331c160afff467cb7
Yi Zhao <yizhao.cs@gmail.com>
2016-05-12 10:57:02 -0700

    improved usermanagement controller style

--- src/main/webapp/static/js/controller/usermanagement/login.controller.js
+++ src/main/webapp/static/js/controller/usermanagement/login.controller.js
@@ -2,39 +2,31 @@
  * @author YI ZHAO[yi.zhao@adara.com]
  */
 
-(function () {
-    'use strict';
+'use strict';
 
-    angular
-        .module('myApp')
-        .controller('LoginController', LoginController);
+app.controller('LoginController', function LoginController($rootScope, $location, AuthenticationService, FlashService) {
+    var vm = this;
+    vm.login = login;
 
-    LoginController.$inject = ['$rootScope', '$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($rootScope, $location, AuthenticationService, FlashService) {
-        var vm = this;
-        vm.login = login;
+    (function initController() {
+        // reset login status
+        $rootScope.isroot = false;
+        AuthenticationService.ClearCredentials();
+    })();
 
-        (function initController() {
-            // reset login status
-            $rootScope.isroot = false;
-            AuthenticationService.ClearCredentials();
-        })();
-
-        function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.status == 'Success') {
-                    if (vm.username == 'root') {
-                        $rootScope.isroot = true;
-                    }
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path($rootScope.base);
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
+    function login() {
+        vm.dataLoading = true;
+        AuthenticationService.Login(vm.username, vm.password, function (response) {
+            if (response.status == 'Success') {
+                if (vm.username == 'root') {
+                    $rootScope.isroot = true;
                 }
-            });
-        };
-    }
-
-})();
+                AuthenticationService.SetCredentials(vm.username, vm.password);
+                $location.path($rootScope.base);
+            } else {
+                FlashService.Error(response.message);
+                vm.dataLoading = false;
+            }
+        });
+    };
+});
--- src/main/webapp/static/js/controller/usermanagement/manageusers.controller.js
+++ src/main/webapp/static/js/controller/usermanagement/manageusers.controller.js
@@ -2,58 +2,50 @@
  * @author YI ZHAO[yi.zhao@adara.com]
  */
 
-(function () {
-    'use strict';
+'use strict';
 
-    angular
-        .module('myApp')
-        .controller('ManageusersController', ManageusersController);
+app.controller('ManageusersController', function ManageusersController(UserService, $rootScope, $location, md5) {
+    var vm = this;
 
-    ManageusersController.$inject = ['UserService', '$rootScope', '$location', 'md5'];
-    function ManageusersController(UserService, $rootScope, $location, md5) {
-        var vm = this;
+    vm.user = null;
+    vm.allUsers = [];
+    vm.deleteUser = deleteUser;
+    vm.updateUser = updateUser;
 
-        vm.user = null;
-        vm.allUsers = [];
-        vm.deleteUser = deleteUser;
-        vm.updateUser = updateUser;
+    initController();
 
-        initController();
-
-        function initController() {
-            loadCurrentUser();
-            loadAllUsers();
-        }
-
-        function loadCurrentUser() {
-            UserService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
-                    vm.user = user;
-                });
-        }
-
-        function loadAllUsers() {
-            UserService.GetAll()
-                .then(function (users) {
-                    vm.allUsers = users;
-                });
-        }
-
-        function deleteUser(id) {
-            UserService.Delete(id)
-                .then(function () {
-                    loadAllUsers();
-                });
-        }
-
-        function updateUser(user) {
-            user.password = md5.createHash(user.password);
-            UserService.Update(user)
-                .then(function () {
-                    $location.path($rootScope.base + 'manageusers');
-                    loadAllUsers();
-                });
-        }
+    function initController() {
+        loadCurrentUser();
+        loadAllUsers();
     }
 
-})();
+    function loadCurrentUser() {
+        UserService.GetByUsername($rootScope.globals.currentUser.username)
+            .then(function (user) {
+                vm.user = user;
+            });
+    }
+
+    function loadAllUsers() {
+        UserService.GetAll()
+            .then(function (users) {
+                vm.allUsers = users;
+            });
+    }
+
+    function deleteUser(id) {
+        UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+    }
+
+    function updateUser(user) {
+        user.password = md5.createHash(user.password);
+        UserService.Update(user)
+            .then(function () {
+                $location.path($rootScope.base + 'manageusers');
+                loadAllUsers();
+            });
+    }
+});
